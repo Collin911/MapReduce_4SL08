d6ece2b271bb79e2655969364e1a41480611d4f6
CollinXia <51706420+Collin911@users.noreply.github.com>
2025-07-03 13:59:20 +0200

    Allow for a maximum of 10 workers

--- src/Config.java
+++ src/Config.java
@@ -11,14 +11,20 @@ public class Config {
     public static final outType OUT = outType.INFO;
 
     // Specify the workers' info here and ONLY HERE
+    // Comment out those not needed
     public static List<NodeInfo> loadWorkers() {
         return List.of(
                 new NodeInfo(0, "tp-1d22-01", 10001)
                 ,new NodeInfo(1, "tp-1d22-02", 10002)
-                ,new NodeInfo(2, "tp-1d22-03", 10003)
-                ,new NodeInfo(3, "tp-1d22-04", 10004)
-                ,new NodeInfo(4, "tp-1d22-05", 10005)
-                ,new NodeInfo(5, "tp-1d22-06", 10006)
+                //,new NodeInfo(2, "tp-1d22-03", 10003)
+                //,new NodeInfo(3, "tp-1d22-04", 10004)
+                //,new NodeInfo(4, "tp-1d22-05", 10005)
+                //,new NodeInfo(5, "tp-1d22-06", 10006)
+                //,new NodeInfo(6, "tp-1d22-08", 10007)
+                //,new NodeInfo(7, "tp-1d22-09", 10008)
+                //,new NodeInfo(8, "tp-1d22-10", 10009)
+                //,new NodeInfo(9, "tp-1d22-11", 10010)
+
         );
     }
 
